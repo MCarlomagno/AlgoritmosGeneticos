b2c06bf6b4a4767d84aa38534f3d6155f6714898
MCarlomagno <marcoscarlomagno1@gmail.com>
2016-04-01 11:37:49 -0300

    actializado, falta plotear graficas de resultados

--- AG_primerTP.py
+++ AG_primerTP.py
@@ -10,6 +10,8 @@ metodo de seleccion = ruleta
 metodo de crossover = 1 punto
 metodo de mutacion = invertida
 """""
+PROB_CROSS = 0.75
+PROB_MUT = 0.05
 
 import random
 
@@ -36,22 +38,100 @@ def cargaFitness(poblacion):
     for cromosoma in poblacion:
         #bucle feo
         valor_entero = 0
+        j = 0
         for i in range(len(cromosoma)-1,-1,-1):
-            if (cromosoma[i]==1):
-                valor_entero += 2**i
+            valor_entero += cromosoma[i]*2**j
+            j+=1
         cromosoma.append(valor_entero) #asigno el entero como un ultimo elemento de la lista
         suma += valor_entero
     #en este bucle calculo el fitness de cada cromosoma y lo cargo a la poblacion
     for cromo in poblacion:
         fitness = cromo[30]/float(suma)
+        cromo.pop(30)
         tupla = (cromo,fitness)
         poblacion[poblacion.index(cromo)] = tupla
-        cromo.pop(30)
     return poblacion
+
+def cargaRuleta(pob):
+    """""
+    para armar la ruleta hacemos una lista de tuplas que le
+    corresponde a cada cromosoma con 3 valores, el inicio del arco
+    de circunferencia, el fin y el cromosoma
+    """""
+    ruleta = []
+    ac = 0.0
+    for cromo in pob:
+        angulo = cromo[1]*360
+        tupla = (ac,ac+angulo,cromo[0])
+        ruleta.append(tupla)
+        ac += angulo
+    return ruleta
+
+def buscaPadre(numero,ruleta):
+    # recibe un numero al azar entre 0 y 360 y devuelve el cromosoma que corresponde al intervalo que contiene el numero
+    for rang in ruleta:
+        if (numero > rang[0])and(numero <= rang[1]):
+            padre = rang[2]
+            break
+    return padre
+
+def crossOver(par):
+    #recibe una lista que contiene el par de padres y devuelve una lista con el par de hijos
+    puntoCorte = random.randint(2,30)
+    cromo1 = par[0]
+    cromo2 = par[1]
+    prefijo1 = cromo1[:puntoCorte-1]
+    sufijo1 = cromo2[puntoCorte-1:]
+    prefijo2 = cromo1[puntoCorte-1:]
+    sufijo2 = cromo2[:puntoCorte-1]
+    hijo1 = prefijo1 + sufijo1
+    hijo2 = prefijo2 + sufijo2
+    nuevoPar = [hijo1,hijo2]
+    return nuevoPar
+
+def mutacion(cromosoma):
+    posgen = random.randint(0,29)
+    if cromosoma[posgen] == 1:
+        cromosoma[posgen] = 0
+    else:
+        cromosoma[posgen] = 1
+    return cromosoma
 
 
 #programa principal
 
 poblacion = iniciarPoblacion()
-pobfitness = cargaFitness(poblacion)
+for k in range(0,20):
+    pobfitness = cargaFitness(poblacion)
+    ruleta = cargaRuleta(pobfitness)
+    padres = []
+    for i in range(0,10):
+        #buscar los 10 padres y agregarlos a una lista de padres
+        num = random.randint(1,1000) #para darle dos decimales
+        seleccion = (num/1000.0)*360
+        padres.append(buscaPadre(seleccion,ruleta))
+
+    acum = 0
+    #se toman de a pares de padres para hacer crossover y asignar a la nuevo poblacion
+    poblacion = []
+    for j in range(0,5):
+        par = padres[acum:acum+2]
+        acum += 2
+        numeroRand = random.randint(1,100)
+        if numeroRand <= (PROB_CROSS*100):
+            nuevoPar = crossOver(par)
+            poblacion.extend(nuevoPar)
+        else:
+            poblacion.extend(par)
+
+    for cromosoma in poblacion:
+        numeroRand2 = random.randint(1,100)
+        if numeroRand2 <= (PROB_MUT*100):
+            poblacion[poblacion.index(cromosoma)] = mutacion(cromosoma)
+
+print(poblacion)
+
+
+
+
 
